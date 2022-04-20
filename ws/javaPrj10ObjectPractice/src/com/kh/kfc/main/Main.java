cbdf739c99c94e699b72d19dc0e62276e3f6232d
DESKTOP-G915MPU\user2 <abc>
2022-04-20 21:28:50 +0900

    객체

--- ws/javaPrj10ObjectPractice/src/com/kh/kfc/main/Main.java
+++ ws/javaPrj10ObjectPractice/src/com/kh/kfc/main/Main.java
@@ -1,6 +1,9 @@
 package com.kh.kfc.main;
 
 import com.kh.kfc.obj.Menu;
+import com.kh.kfc.obj.MenuBeverage;
+import com.kh.kfc.obj.MenuHamburger;
+import com.kh.kfc.obj.MenuSide;
 
 public class Main {
 
@@ -8,7 +11,23 @@ public class Main {
 		
 		System.out.println("KFC 에 오신걸 환영합니다. ");
 		
-		Menu m = new Menu();
+		// 메뉴판 객체 생성
+		Menu menu = new Menu();
+		MenuHamburger menuHam = new MenuHamburger();
+		MenuBeverage menuBev = new MenuBeverage();
+		MenuSide menuSide = new MenuSide();
+		
+		int menuNum = menu.showMenu();
+		
+		if(menuNum == 1) {
+			menuHam.showMenuHam();
+		}else if(menuNum == 2) {
+			menuBev.showMenuBeverage();
+		}else if(menuNum == 3) {
+			menuSide.showMenuSide();
+		}else {
+			System.out.println("숫자 잘못 누름");
+		}
 
 	}
 
