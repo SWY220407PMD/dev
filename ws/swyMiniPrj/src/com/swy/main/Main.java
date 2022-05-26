862def60528e0bec4f66b3bf01497fea4be29758
swy <swy@yaho.zzz>
2022-05-26 16:33:36 +0900

    메인 반복문 씌움

--- ws/swyMiniPrj/src/com/swy/main/Main.java
+++ ws/swyMiniPrj/src/com/swy/main/Main.java
@@ -10,23 +10,25 @@ public class Main {
 		
 		System.out.println("swy mini prj");
 		
-		System.out.println("==================");
-		System.out.println("1. 회원가입");
-		System.out.println("2. 로그인");
-		System.out.println("3. 게시글 작성");
-		System.out.println("4. 게시글 조회");
-		System.out.println("5. 555");
-		
-		int n = MyUtil.scInt();
-		
-		switch(n) {
-		case 1 : new Member().join(); break;
-		case 2 : new Member().login(); break;
-		case 3 : new Board().write(); break;
-		case 4 : new Board().showList(); break;
-		case 5 :  break;
-		default : System.out.println("다시 선택하세요");
-		}
+		while(true) {
+			System.out.println("==================");
+			System.out.println("1. 회원가입");
+			System.out.println("2. 로그인");
+			System.out.println("3. 게시글 작성");
+			System.out.println("4. 게시글 조회");
+			System.out.println("5. 555");
+			
+			int n = MyUtil.scInt();
+			
+			switch(n) {
+			case 1 : new Member().join(); break;
+			case 2 : new Member().login(); break;
+			case 3 : new Board().write(); break;
+			case 4 : new Board().showList(); break;
+			case 5 :  break;
+			default : System.out.println("다시 선택하세요");
+			}
+		}//while
 		
 		
 
